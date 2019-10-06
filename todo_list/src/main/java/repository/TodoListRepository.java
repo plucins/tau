f42497652e67d0f3ae9152531806c74a4c6a8da7
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2019-10-06 11:05:51 +0200

    init comit

---
+++ todo_list/src/main/java/repository/TodoListRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+import model.TodoList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoListRepository {
+
+    private List<TodoList> todoLists;
+
+    public TodoListRepository() {
+        todoLists = new ArrayList<TodoList>();
+    }
+
+    
+}
---
+++ todo_list/src/main/java/service/TodoListService.java
@@ -0,0 +1,15 @@
+package service;
+
+import model.TodoTask;
+
+public class TodoListService {
+
+    public TodoListService() {
+
+    }
+
+
+    protected boolean addTaskToList(TodoTask task){
+
+    }
+}
